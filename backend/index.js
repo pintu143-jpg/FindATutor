1794c79519563f48ee0ccf93c8991278b00eaadd
pintu143 <pintukumar3415@gmail.com>
2026-01-05 04:55:26 +0530

    Refactor: Separate project into frontend and backend directories

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.join(__dirname, '../.env') });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
